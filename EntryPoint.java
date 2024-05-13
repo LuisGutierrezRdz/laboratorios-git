79a5f1817fdf669d449271f79caf7c657750f79c
darklatiz <software.j2ee@gmail.com>
2024-05-13 13:36:52 -0500

    Cambios en main que causarán conflicto

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,5 +1,8 @@
 public class EntryPoint{
 	public static void main(String[] args) {
+		for (int j ; j < 1000; j++) {
+			System.out.println("Calculando la masa del sol....");
+		}
 		System.out.println("Hola");
 	}
 }
